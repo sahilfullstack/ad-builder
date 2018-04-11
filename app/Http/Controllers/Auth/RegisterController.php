43a4f88dd2c246143ee990702a255c9dcc047c55
Mohit Mamoria <mohit.mamoria@gmail.com>
2018-04-11 14:45:33 +0000

    fixed the redirects

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -27,7 +27,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/units';
 
     /**
      * Create a new controller instance.
