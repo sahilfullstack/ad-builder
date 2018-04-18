6f0df7dd294b1159634d77ab59291ea609fcb675
vagrant <vagrant@homestead>
2018-04-18 08:59:41 +0000

    layout migration added

---
+++ database/migrations/2018_04_18_072453_create_layouts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLayoutsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('layouts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('slug');
+            $table->integer('template_id');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->unsignedBigInteger('deleted_at_millis')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('layouts');
+    }
+}
