dce9e3b774987afbb1030bdd16ac306a38ac04cd
Mohit Mamoria <mohit.mamoria@gmail.com>
2018-04-25 11:05:30 +0000

    removed redundant namespace declaration

--- app/Http/Controllers/Api/TemplateController.php
+++ app/Http/Controllers/Api/TemplateController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Template;
 use App\Models\Component;
-use App\Models\Template;
 use App\Http\Requests\StoreTemplateRequest;
 use App\Exceptions\InvalidInputException;
 
