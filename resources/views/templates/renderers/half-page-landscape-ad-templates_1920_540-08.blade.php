c0bb808ce98f35154f97d58ff09c4ba6ee897f86
Mohit Mamoria <mohit.mamoria@gmail.com>
2018-04-26 09:38:11 +0000

    made half page landscape templates

---
+++ resources/views/templates/renderers/half-page-landscape-ad-templates_1920_540-08.blade.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        /* Reset Begins */
+        html, body, ul, ol, li, form, fieldset, legend
+        {
+            margin: 0;
+            padding: 0;
+        }
+
+        h1, h2, h3, h4, h5, h6, p { margin-top: 0; }
+
+        fieldset,img { border: 0; }
+
+        legend { color: #000; }
+
+        li { list-style: none; }
+
+        sup { vertical-align: text-top; }
+
+        sub { vertical-align: text-bottom; }
+
+        table
+        {
+            border-collapse: collapse;
+            border-spacing: 0;
+        }
+
+        caption, th, td
+        {
+            text-align: left;
+            vertical-align: top;
+            font-weight: normal;
+        }
+
+        input, textarea, select
+        {
+            font-size: 110%;
+            line-height: 1.1;
+        }
+
+        abbr, acronym
+        {
+            border-bottom: .1em dotted;
+            cursor: help;
+        }
+        /* Reset Ends */
+
+        html {
+            box-sizing: border-box;
+        }
+        *, *:before, *:after {
+            box-sizing: inherit;
+        }
+        #workspace {
+            background: #ccc;
+            width: 960px;
+            height: 540px;
+            font-family: sans-serif;
+        }
+
+        div.body {
+            width: 960px;
+            height: 260px;
+            background: red;
+        }
+
+        div.banner {
+            width: 960px;
+            height: 10px;
+            /* position: absolute; */
+            /* top: 0;
+            left: 0; */
+            background: #199FD4;
+        }
+
+        div.sidebar {
+            width: 310px;
+            float: right;
+            height: 260px;
+            background: #FFFFFF;
+            padding-left: 15px;
+        }
+
+        div.sidebar p.category-header {
+            padding-top: 10px;
+            color: #199FD4;
+            font-size: 16px;
+            font-weight: bold;
+            text-transform: uppercase;
+        }
+
+        div.sidebar div.logo {
+            width: 270px;
+            height: 60px;
+            margin: 20px auto 20px;
+        }
+
+        div.sidebar div.logo img {
+            width: 270px;
+            height: 60px;
+        }
+
+        div.sidebar div.logo div.logo-placeholder {
+            width: 270px;
+            height: 60px;
+            outline: 3px dotted #CC337A;
+        }
+
+        div.sidebar div.logo div.logo-placeholder p {
+            color: #CC337A;
+            text-transform: uppercase;
+            font-weight: bold;
+            text-align: center;
+            padding-top: 25px;
+        }
+        
+        div.sidebar div.quote {
+            width: 270px;
+            height: 50px;
+            background: #199FD4;
+            margin: 0 auto 30px;
+            position: relative;
+        }
+
+        div.flag {
+            width: 0px;
+            height: 0px;
+            position: absolute;
+            bottom: -20px;
+            right: 0px;
+            border-left: 20px solid transparent;
+            border-right: 20px solid transparent;
+            border-top: 20px solid #199FD4;
+        }
+
+        div.sidebar div.text {
+            width: 200px;
+            height: 42.5px;
+            background: #C6E5F3;
+            margin-left: 10px;
+        }
+
+        div.hero-video {
+            height: 260px;
+        }
+
+        div.hero-video img {
+            width: 650px;
+            height: 260px;
+        }
+
+        div.hero-video div.hero-video-placeholder {
+            height: 260px;
+            background: #C6E5F3;
+        }
+
+        p.null-state {
+            padding-top: 230px;
+            text-transform: uppercase;
+            font-weight: bold;
+            font-size: 18px;
+            color: #aaa;
+            text-align: center;
+        }
+    </style>
+    
+</head>
+<body>
+    
+    <div id="workspace">
+        <div class="banner"></div>
+
+        <div class="body">
+            <div class="sidebar">
+                <p class="category-header">Category Header</p>
+
+                <div class="logo">
+                    <div class="logo-placeholder">
+                        <p>LOGO</p>
+                    </div>
+                </div>
+
+                <div class="quote">
+                    <div class="flag"></div>
+                    <p>Quote Text</p>
+                </div>
+
+                <div class="text">
+                    <p>Text</p>
+                </div>
+            </div>
+    
+            <div class="hero-video">
+                <div class="hero-video-placeholder">
+                    Hero Video
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
