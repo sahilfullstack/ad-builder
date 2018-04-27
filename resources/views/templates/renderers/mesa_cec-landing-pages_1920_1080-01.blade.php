c1513e56941e0557d3db6aa0a7daa8b25cd96d1b
Mohit Mamoria <mohit.mamoria@gmail.com>
2018-04-27 11:39:26 +0000

    made two landing page templates

---
+++ resources/views/templates/renderers/mesa_cec-landing-pages_1920_1080-01.blade.php
@@ -0,0 +1,227 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        /* Reset Begins */
+        html, body, ul, ol, li, form, fieldset, legend
+        {
+            margin: 0;
+            padding: 0;
+        }
+
+        h1, h2, h3, h4, h5, h6, p { margin-top: 0; }
+
+        fieldset,img { border: 0; }
+
+        legend { color: #000; }
+
+        li { list-style: none; }
+
+        sup { vertical-align: text-top; }
+
+        sub { vertical-align: text-bottom; }
+
+        table
+        {
+            border-collapse: collapse;
+            border-spacing: 0;
+        }
+
+        caption, th, td
+        {
+            text-align: left;
+            vertical-align: top;
+            font-weight: normal;
+        }
+
+        input, textarea, select
+        {
+            font-size: 110%;
+            line-height: 1.1;
+        }
+
+        abbr, acronym
+        {
+            border-bottom: .1em dotted;
+            cursor: help;
+        }
+        /* Reset Ends */
+
+        html {
+            box-sizing: border-box;
+        }
+        *, *:before, *:after {
+            box-sizing: inherit;
+        }
+        #workspace {
+            background: #ccc;
+            width: 960px;
+            height: 540px;
+            font-family: sans-serif;
+
+            /*background-image: url(http://chrl.test/temp/MESA_CEC%20Landing%20Pages_1920_1080-01.jpg);*/
+            /*background-size: 960px 540px;*/
+        }
+
+        div.banner {
+            width: 960px;
+            height: 10px;
+            background: #199FD4;
+        }
+
+        div.body {
+            width: 960px;
+            height: 540px;
+            background: #fff;
+
+            position: relative;
+            overflow: auto;
+        }
+
+        div.logo {
+            width: 204px;
+            height: 72px;
+            position: absolute;
+            top: 34px;
+            left: 23px
+        }
+
+        div.logo img {
+            width: 204px;
+            height: 72px;
+        }
+
+        div.logo div.logo-placeholder {
+            width: 204px;
+            height: 72px;
+            outline: 3px dotted #CC337A;
+        }
+
+        div.logo div.logo-placeholder p {
+            color: #CC337A;
+            text-transform: uppercase;
+            font-weight: bold;
+            text-align: center;
+            padding-top: 35px;
+        }
+
+        div.title {
+            width: 690px;
+            height: 68px;
+            position: absolute;
+            top: 40px;
+            left: 258px;
+            font-size: 27px;
+            padding-top: 18px;
+            padding-left: 7px;
+            color: #29aae2;
+        }
+
+        div.hero-image {
+            width: 385px;
+            height: 383px;
+            position: absolute;
+            top: 131px;
+            left: 20px;
+        }
+        div.hero-image img {
+            width: 385px;
+            height: 383px;
+        }
+        div.hero-image div.hero-image-placeholder {
+            width: 385px;
+            height: 383px;
+            background: #C6E5F3;
+        }
+
+        div#text-1 {
+            width: 531px;
+            height: 134px;
+            background: #C6E5F3;
+            position: absolute;
+            top: 131px;
+            left: 414px;
+        }
+        div#text-2 {
+            width: 186px;
+            height: 128px;
+            background: #C6E5F3;
+            position: absolute;
+            top: 276px;
+            left: 414px;
+        }
+
+        div.survey {
+            width: 335px;
+            height: 128px;
+            background: #C6E5F3; 
+            position: absolute;
+            top: 276px;
+            left: 610px;
+        }
+
+        div.map {
+            width: 187px;
+            height: 102px;
+            background: #C6E5F3;
+            position: absolute;
+            top: 412px;
+            left: 414px;
+        }
+        div.map img {
+            width: 187px;
+            height: 102px;
+        }
+        div.map div.map-placeholder {
+            width: 187px;
+            height: 102px;
+            background: #C6E5F3;
+        }
+    </style>
+    
+</head>
+<body>
+    
+    <div id="workspace">
+        <div class="banner"></div>
+
+        <div class="body">
+            <div class="logo">
+                <div class="logo-placeholder">
+                    <p>LOGO</p>
+                </div>
+            </div>
+            <div class="title">
+                <h1>Landing Page Title</h1>
+            </div>
+
+            <div class="hero-image">
+                <div class="hero-image-placeholder">
+                    Hero Image
+                </div>
+            </div>
+
+            <div id="text-1">
+                <p>Text 1</p>
+            </div>
+
+            <div id="text-2">
+                <p>Text 2</p>
+            </div>
+
+            <div class="survey">
+                <p>Survey</p>
+            </div>
+
+            <div class="map">
+                <div class="map-placeholder">
+                    Map
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ resources/views/templates/renderers/mesa_cec-landing-pages_1920_1080-02.blade.php
@@ -0,0 +1,225 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        /* Reset Begins */
+        html, body, ul, ol, li, form, fieldset, legend
+        {
+            margin: 0;
+            padding: 0;
+        }
+
+        h1, h2, h3, h4, h5, h6, p { margin-top: 0; }
+
+        fieldset,img { border: 0; }
+
+        legend { color: #000; }
+
+        li { list-style: none; }
+
+        sup { vertical-align: text-top; }
+
+        sub { vertical-align: text-bottom; }
+
+        table
+        {
+            border-collapse: collapse;
+            border-spacing: 0;
+        }
+
+        caption, th, td
+        {
+            text-align: left;
+            vertical-align: top;
+            font-weight: normal;
+        }
+
+        input, textarea, select
+        {
+            font-size: 110%;
+            line-height: 1.1;
+        }
+
+        abbr, acronym
+        {
+            border-bottom: .1em dotted;
+            cursor: help;
+        }
+        /* Reset Ends */
+
+        html {
+            box-sizing: border-box;
+        }
+        *, *:before, *:after {
+            box-sizing: inherit;
+        }
+        #workspace {
+            background: #ccc;
+            width: 960px;
+            height: 540px;
+            font-family: sans-serif;
+
+            background-image: url(http://chrl.test/temp/MESA_CEC%20Landing%20Pages_1920_1080-02.jpg);
+            background-size: 960px 540px;
+        }
+
+        div.banner {
+            width: 960px;
+            height: 10px;
+            background: #199FD4;
+        }
+
+        div.body {
+            width: 960px;
+            height: 540px;
+            background: transparent;
+
+            position: relative;
+            overflow: auto;
+        }
+
+        div.logo {
+            width: 192px;
+            height: 47px;
+            position: absolute;
+            top: 22px;
+            left: 22px;
+        }
+
+        div.logo img {
+            width: 192px;
+            height: 47px;
+        }
+
+        div.logo div.logo-placeholder {
+            width: 192px;
+            height: 47px;
+            outline: 3px dotted #CC337A;
+        }
+
+        div.logo div.logo-placeholder p {
+            color: #CC337A;
+            text-transform: uppercase;
+            font-weight: bold;
+            text-align: center;
+            padding-top: 15px;
+        }
+
+        div.title {
+            width: 690px;
+            height: 68px;
+            position: absolute;
+            top: 62px;
+            left: 13px;
+            font-size: 19px;
+            padding-top: 18px;
+            padding-left: 7px;
+            color: #29aae2;
+        }
+
+        div.hero-image {
+            width: 603px;
+            height: 246px;
+            position: absolute;
+            top: 131px;
+            left: 21px;
+        }
+        div.hero-image img {
+            width: 603px;
+            height: 246px;
+        }
+        div.hero-image div.hero-image-placeholder {
+            width: 603px;
+            height: 246px;
+            background: #C6E5F3;
+        }
+
+        div.hero-video {
+            width: 308px;
+            height: 246px;
+            position: absolute;
+            top: 131px;
+            left: 638px;
+        }
+        div.hero-video img {
+            width: 308px;
+            height: 246px;
+        }
+        div.hero-video div.hero-video-placeholder {
+            width: 308px;
+            height: 246px;
+            background: #C6E5F3;
+        }
+
+        div#text-1 {
+            width: 465px;
+            height: 134px;
+            background: #C6E5F3;
+            position: absolute;
+            top: 384px;
+            left: 21px;
+        }
+
+        div.timeline {
+            width: 443px;
+            height: 134px;
+            background: #C6E5F3;
+            position: absolute;
+            top: 385px;
+            left: 503px;
+        }
+        div.timeline img {
+            width: 443px;
+            height: 134px;
+        }
+        div.timeline div.timeline-placeholder {
+            width: 443px;
+            height: 134px;
+            background: #C6E5F3;
+        }
+    </style>
+    
+</head>
+<body>
+    
+    <div id="workspace">
+        <div class="banner"></div>
+
+        <div class="body">
+            <div class="logo">
+                <div class="logo-placeholder">
+                    <p>LOGO</p>
+                </div>
+            </div>
+            <div class="title">
+                <h1>Landing Page Title</h1>
+            </div>
+
+            <div class="hero-image">
+                <div class="hero-image-placeholder">
+                    Hero Image
+                </div>
+            </div>
+
+            <div class="hero-video">
+                <div class="hero-video-placeholder">
+                    Hero Video
+                </div>
+            </div>
+
+            <div id="text-1">
+                <p>Text 1</p>
+            </div>
+
+            <div class="timeline">
+                <div class="timeline-placeholder">
+                    Timeline
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
