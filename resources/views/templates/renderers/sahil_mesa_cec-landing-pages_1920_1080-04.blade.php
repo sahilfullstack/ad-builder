d4d59884566f031c469a83d6c56303d9548c5cae
vagrant <vagrant@homestead>
2018-05-02 13:55:05 +0000

    view 4 is perfect now

---
+++ resources/views/templates/renderers/sahil_mesa_cec-landing-pages_1920_1080-04.blade.php
@@ -0,0 +1,351 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        /* Reset Begins */
+        html, body, ul, ol, li, form, fieldset, legend
+        {
+            margin: 0;
+            padding: 0;
+        }
+
+        h1, h2, h3, h4, h5, h6, p { margin-top: 0; }
+
+        fieldset,img { border: 0; }
+
+        legend { color: #000; }
+
+        li { list-style: none; }
+
+        sup { vertical-align: text-top; }
+
+        sub { vertical-align: text-bottom; }
+
+        table
+        {
+            border-collapse: collapse;
+            border-spacing: 0;
+        }
+
+        caption, th, td
+        {
+            text-align: left;
+            vertical-align: top;
+            font-weight: normal;
+        }
+
+        input, textarea, select
+        {
+            font-size: 110%;
+            line-height: 1.1;
+        }
+
+        abbr, acronym
+        {
+            border-bottom: .1em dotted;
+            cursor: help;
+        }
+        /* Reset Ends */
+
+        html {
+            box-sizing: border-box;
+        }
+        *, *:before, *:after {
+            box-sizing: inherit;
+        }
+        #workspace {
+            background: #ccc;
+            width: 1920px;
+            height: 1080px;
+            font-family: sans-serif;
+
+            /*background-image: url(http://chrl.test/temp/MESA_CEC%20Landing%20Pages_1920_1080-01.jpg);*/
+            /*background-size: 1920px 1080px;*/
+        }
+
+        div.banner {
+            width: 1920px;
+            height: 26.775px;
+            background: #199FD4;
+        }
+
+        div.body {
+            width: 1920px;
+            height: 1080px;
+            background: #fff;
+
+            position: relative;
+            overflow: auto;
+        }
+
+        div.hero-image1 {
+            width: 658.308px;
+            height: 319.18px;
+            position: absolute;
+            top: 37px;
+            left: 29px;
+        }
+        div.hero-image1 img {
+            width: 658.308px;
+            height: 319.18px;
+        }
+        div.hero-image1 div.hero-image1-placeholder {
+            width: 658.308px;
+            height: 319.18px;
+            background: #C6E5F3;
+        }  
+
+        div.hero-image2 {
+            width: 658.308px;
+            height: 319.18px;
+            position: absolute;
+            top: 380.955px;
+            left: 29px;
+        }
+        div.hero-image2 img {
+            width: 658.308px;
+            height: 319.18px;
+        }
+        div.hero-image2 div.hero-image2-placeholder {
+            width: 658.308px;
+            height: 319.18px;
+            background: #C6E5F3;
+        }  
+
+        div.hero-image3 {
+            width: 658.308px;
+            height: 319.18px;
+            position: absolute;
+            top: 730.775px;
+            left: 29px;
+        }
+        div.hero-image3 img {
+            width: 658.308px;
+            height: 319.18px;
+        }
+        div.hero-image3 div.hero-image3-placeholder {
+            width: 658.308px;
+            height: 319.18px;
+            background: #C6E5F3;
+        }
+
+        div.hero-image4 {
+            width: 414px;
+            height: 207px;
+            position: absolute;
+            top: 320px;
+            left: 720px;
+        }
+        div.hero-image4 img {
+            width: 414px;
+            height: 207px;
+        }
+        div.hero-image4 div.hero-image4-placeholder {
+            width: 414px;
+            height: 207px;
+            background: #C6E5F3;
+        }
+        
+        div.hero-image5 {
+            width: 1017px;
+            height: 76px;
+            position: absolute;
+            top: 590px;
+            left: 720px;
+        }
+        div.hero-image5 img {
+            width: 1017px;
+            height: 76px;
+        }
+        div.hero-image5 div.hero-image5-placeholder {
+            width: 1017px;
+            height: 76px;
+            background: #C6E5F3;
+        }
+
+        div.hero-image6 {
+            width: 1147.858px;
+            height: 315.815px;
+            position: absolute;
+            top: 730px;
+            left: 720px;
+        }
+        div.hero-image6 img {
+            width: 1147.858px;
+            height: 315.815px;
+        }
+        div.hero-image6 div.hero-image6-placeholder {
+            width: 1147.858px;
+            height: 315.815px;
+            background: #C6E5F3;
+        }
+
+                div.social-qrs-1 {
+            width: 114.5px;
+            height: 110.796px;
+            background: #C6E5F3;
+            position: absolute;
+            top: 106px;
+            left: 1420px;
+            display: flex;
+            justify-content: space-evenly;
+            align-items: center;
+        }
+        div.social-qrs-2 {
+            width: 114.5px;
+            height: 110.796px;
+            background: #C6E5F3;
+            position: absolute;
+            top: 106px;
+            left: 1540px;
+            display: flex;
+            justify-content: space-evenly;
+            align-items: center;
+        }
+        div.social-qrs-3 {
+            width: 114.5px;
+            height: 110.796px;
+            background: #C6E5F3;
+            position: absolute;
+            top: 106px;
+            left: 1660px;
+            display: flex;
+            justify-content: space-evenly;
+            align-items: center;
+        }
+
+        div.social-qrs div.social-qr {
+            width: 50px;
+            height: 50px;
+            background: rgba(255,0,0,0.2);
+        }
+
+
+           div.social-logo-image1 {
+            width: 63.361px;
+            height: 43.552px;
+            position: absolute;
+            top: 46px;
+            left: 1440px;
+        }
+        div.social-logo-image1 img {
+            width: 63.361px;
+            height: 43.552px;
+        }
+        div.social-logo-image1 div.social-logo-image1-placeholder {
+            width: 63.361px;
+            height: 43.552px;
+            background: #C6E5F3;
+        }
+
+        div.social-logo-image2 {
+            width: 63.361px;
+            height: 43.552px;
+            position: absolute;
+            top: 46px;
+            left: 1570px;
+        }
+        div.social-logo-image2 img {
+            width: 63.361px;
+            height: 43.552px;
+        }
+        div.social-logo-image2 div.social-logo-image2-placeholder {
+            width: 63.361px;
+            height: 43.552px;
+            background: #C6E5F3;
+        }
+
+        div.social-logo-image3 {
+            width: 63.361px;
+            height: 43.552px;
+            position: absolute;
+            top: 46px;
+            left: 1680px;
+        }
+        div.social-logo-image3 img {
+            width: 63.361px;
+            height: 43.552px;
+        }
+        div.social-logo-image3 div.social-logo-image3-placeholder {
+            width: 63.361px;
+            height: 43.552px;
+            background: #C6E5F3;
+        }
+
+    </style>
+    
+</head>
+<body>
+    
+    <div id="workspace">
+        <div class="banner"></div>
+
+        <div class="body">
+            <div class="hero-image1">
+                <div class="hero-image1-placeholder">
+                    Hero Image 1 
+                </div>
+            </div>
+            <div class="hero-image2">
+                <div class="hero-image2-placeholder">
+                    Hero Image 2
+                </div>                
+            </div>            
+            <div class="hero-image3">
+                <div class="hero-image3-placeholder">
+                    Hero Image 3
+                </div>
+            </div>
+            <div class="hero-image4">
+                <div class="hero-image4-placeholder">
+                    Hero Image 4
+                </div>
+            </div>
+
+            <div class="hero-image5">
+                <div class="hero-image5-placeholder">
+                    Hero Image 5
+                </div>
+            </div>
+            <div class="hero-image6">
+                <div class="hero-image6-placeholder">
+                    Hero Image 6
+                </div>
+            </div>
+                     <div class="social-logo-image1">
+                <div class="social-logo-image1-placeholder">
+                    1
+                </div>
+            </div><div class="social-logo-image2">
+                <div class="social-logo-image2-placeholder">
+                    2
+                </div>
+            </div><div class="social-logo-image3">
+                <div class="social-logo-image3-placeholder">
+                    3
+                </div>
+            </div>
+
+            <div class="social-qrs-1">
+                <div class="social-qr">
+                    
+                </div>
+            </div>
+
+            <div class="social-qrs-2">
+                <div class="social-qr">
+                    
+                </div>
+            </div>
+
+            <div class="social-qrs-3">
+                <div class="social-qr">
+                    
+                </div>
+            </div>
+    </div>
+</body>
+</html>
