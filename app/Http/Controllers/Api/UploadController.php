04963332dd74b2fa6aae8ee27eae473d9162cb84
vagrant <vagrant@homestead>
2018-05-15 17:17:59 +0000

    octel format working properly

--- app/Http/Controllers/Api/UploadController.php
+++ app/Http/Controllers/Api/UploadController.php
@@ -13,13 +13,12 @@ class UploadController extends Controller
 {
     public function upload(UploadRequest $request, FileUploader $fileUploader)
     {
-        \Log::info($request->all());
         try {
             if($request->file->isValid())
             {
                 if($request->file->getMimeType() == "application/octet-stream")
                 {
-                    $url = Storage::url($request->file->storeAs(config('uploads.folder'), md5(microtime()),  ['mimetype' => $request->type]));
+                    $url = Storage::url($request->file->storeAs(config('uploads.folder'), md5(microtime()).".".explode('/', $request->get('type'))[1]));
                 }
                 else
                 {                    
