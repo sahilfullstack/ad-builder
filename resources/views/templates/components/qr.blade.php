989f440398009da5f187040986298c645986b26a
Mohit Mamoria <mohit.mamoria@gmail.com>
2018-06-02 08:28:40 +0000

    made the social icons amazing

--- resources/views/templates/components/qr.blade.php
+++ resources/views/templates/components/qr.blade.php
@@ -1,6 +1,13 @@
 @php
     if(is_array($value) && array_key_exists('_value', $value)) $value = $value['_value'];
     if( ! isset($size)) $size = 200;
+
+    $qr = new chillerlan\QRCode\QRCode(new chillerlan\QRCode\QROptions([
+        'version'    => 5,
+        'outputType' => chillerlan\QRCode\QRCode::OUTPUT_MARKUP_SVG,
+        'eccLevel'   => chillerlan\QRCode\QRCode::ECC_L,
+        'scale'      => 2
+    ]));
 @endphp
 
 @if(empty($value))
@@ -8,5 +15,6 @@
         <p>{{ Illuminate\Support\Str::title($default) }}</p>
     </div>
 @else
-    {!! QrCode::size($size)->generate($value) !!}
+    {!! $qr->render($value) !!}
+    {{-- {!! QrCode::size($size)->generate($value) !!} --}}
 @endif
