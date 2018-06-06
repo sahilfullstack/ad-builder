6cb5624e7bad734cef74c79c3fd43719672080f5
vagrant <vagrant@homestead>
2018-06-06 15:07:34 +0000

    added timeline in make dumm unit commands

--- app/Console/Commands/MakeDummyUnitCommand.php
+++ app/Console/Commands/MakeDummyUnitCommand.php
@@ -67,12 +67,13 @@ class MakeDummyUnitCommand extends Command
             }
 
             Unit::create([
-                'name'        => $this->faker->sentence(5),
-                'user_id'     => $user->id,
-                'template_id' => $template->id,
-                'components'  => $componentValues,
-                'layout_id'   => $template->layout_id,
-                'type'        => 'ad'
+                'name'                    => $this->faker->sentence(5),
+                'user_id'                 => $user->id,
+                'template_id'             => $template->id,
+                'components'              => $componentValues,
+                'experimental_components' => $componentValues,
+                'layout_id'               => $template->layout_id,
+                'type'                    => 'ad'
             ]);
 
             $this->info("unit created for index ". $i);
@@ -92,6 +93,34 @@ class MakeDummyUnitCommand extends Command
         if(in_array($component->type, ["images", "survey"])) return [ ["_value" => $this->dummyImage()], ["_value" => $this->dummyImage()]];
 
         if($component->type == 'color') return ["_value" => sprintf('#%06X', mt_rand(0, 0xFFFFFF))];
+        if($component->type == 'timeline') return ['_value' => [
+                    'title' => $this->faker->sentence(10),
+                    'values' => [
+                        [
+                            'month' => 'June',
+                            'year' => '2018',
+                            'description' => $this->faker->sentence(10),
+                            'image' => $this->dummyImage()
+                        ],
+                      [
+                            'month' => 'July',
+                            'year' => '2018',
+                            'description' => $this->faker->sentence(10),
+                            'image' => $this->dummyImage()
+                        ],
+                        [
+                            'month' => 'August',
+                            'year' => '2018',
+                            'description' => $this->faker->sentence(10),
+                            'image' => $this->dummyImage()
+                        ],[
+                            'month' => 'September',
+                            'year' => '2018',
+                            'description' => $this->faker->sentence(10),
+                            'image' => $this->dummyImage()
+                        ],
+                    ]
+                ]];
     }
 
     private function dummyImage()
