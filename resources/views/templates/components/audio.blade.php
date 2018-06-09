e6b5ac37ae70f0a74f8de3ca47702de2df7d98d4
vagrant <vagrant@homestead>
2018-06-09 14:05:30 +0000

    working fine

--- resources/views/templates/components/audio.blade.php
+++ resources/views/templates/components/audio.blade.php
@@ -1,19 +1,20 @@
 @php
-    if(is_array($value) && array_key_exists('_value', $value)) $value = $value['_value'];
+    if(is_array($value) && array_key_exists('_value', $value)) $originalValue = $value['_value'];
+    if(is_array($value) && array_key_exists('converted_value', $value)) $convertedValue = $value['converted_value'];
 @endphp
 
-@if(empty($value))
+@if(empty($originalValue))
     <div class="audio-placeholder">
         <p>{{ Illuminate\Support\Str::title($default) }}</p>
     </div>
 @else
     @if(is_null(request()->input('relative')))
         <audio autoplay loop preload="auto"
-            src="{{ $value }}"
+            src="{{ $originalValue }}"
         >
     @else
         <audio autoplay loop preload="auto"
-            src="{{ absolute_to_relative_url($value) }}"
+            src="{{ absolute_to_relative_url($convertedValue) }}"
         >
     @endif
 @endif
