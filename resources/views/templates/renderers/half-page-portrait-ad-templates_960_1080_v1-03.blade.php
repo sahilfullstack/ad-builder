9c413395818139ba9cba7761342304916683ba8b
Mohit Mamoria <mohit.mamoria@gmail.com>
2018-06-18 09:44:00 +0000

    added text alignment to the half page portrait ads

--- resources/views/templates/renderers/half-page-portrait-ad-templates_960_1080_v1-03.blade.php
+++ resources/views/templates/renderers/half-page-portrait-ad-templates_960_1080_v1-03.blade.php
@@ -178,9 +178,9 @@
             <div class="text" style="
                 background-color: {{ empty(array_get($readableComponents, 'text')['background_color']) ? 'transparent' : array_get($readableComponents, 'text')['background_color'] }};
                 display: flex;
-                align-items: {{ align_to_flex_rule(array_get($readableComponents, 'text')['valign']) }};
-                justify-content: {{ align_to_flex_rule(array_get($readableComponents, 'text')['halign']) }};
-                text-align: {{ array_get($readableComponents, 'text')['halign'] }};
+                align-items: {{ align_to_flex_rule(array_get($readableComponents, 'text.valign')) }};
+                justify-content: {{ align_to_flex_rule(array_get($readableComponents, 'text.halign')) }};
+                text-align: {{ array_get($readableComponents, 'text.halign') }};
             ">
                 @include('templates.components.text', ['value' => array_get($readableComponents, 'text'), 'default' => 'Text'])
             </div>
